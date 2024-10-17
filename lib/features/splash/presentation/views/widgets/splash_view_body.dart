e123430665de8b6595b67ad08c393233bb3046ed
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-10-17 19:05:16 +0200

    Enabled responsive and adaptive UI in SplashView

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zen_tasker/core/helper_function/is_tablet.dart';
 import 'package:zen_tasker/core/utils/app_images.dart';
 import 'package:zen_tasker/core/widgets/custom_title_text.dart';
 
@@ -13,14 +14,15 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(
           AppImages.imagesLogoApp,
-          width: 150,
-          height: 150,
+          width: isMobile(context) ? 200 : 350,
+          height: isMobile(context) ? 200 : 350,
+          fit: BoxFit.contain,
         ),
         const SizedBox(
           height: 24,
         ),
-        const CustomTitleText(
-          fontSize: 35,
+        CustomTitleText(
+          fontSize: isMobile(context) ? 35 : 50,
         ),
       ],
     );
