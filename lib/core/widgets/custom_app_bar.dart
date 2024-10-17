371f803a9ba0fd766147a9c54c5775f52ab64fdc
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-10-17 17:51:16 +0200

    Extract CustomAppBar

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:zen_tasker/core/helper_function/is_tablet.dart';
+import 'package:zen_tasker/core/utils/app_styles.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      toolbarHeight: isMobile(context) ? null : 80,
+      title: Text(
+        title,
+        style: isMobile(context)
+            ? AppStyles.styleBold18(context)
+            : AppStyles.styleBold35(context),
+      ),
+      centerTitle: true,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(
+          Icons.arrow_back_ios_new_rounded,
+          color: Colors.black,
+          size: isMobile(context) ? null : 40,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
+}
--- lib/features/create_task/presentation/views/create_task_view.dart
+++ lib/features/create_task/presentation/views/create_task_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:zen_tasker/core/helper_function/is_tablet.dart';
 import 'package:zen_tasker/core/services/get_it_service.dart';
-import 'package:zen_tasker/core/utils/app_styles.dart';
+import 'package:zen_tasker/core/widgets/custom_app_bar.dart';
 import 'package:zen_tasker/features/create_task/data/repos/create_task_repo.dart';
 import 'package:zen_tasker/features/create_task/presentation/managers/create_task_cubit/create_task_cubit.dart';
 import 'package:zen_tasker/features/create_task/presentation/views/widgets/create_task_view_body_bloc.dart';
@@ -20,28 +19,10 @@ class CreateTaskView extends StatelessWidget {
         getIt.get<CreateTaskRepo>(),
       ),
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          toolbarHeight: isMobile(context) ? null : 80,
-          title: Text(
-            S.of(context).addTask,
-            style: isMobile(context)
-                ? AppStyles.styleBold18(context)
-                : AppStyles.styleBold35(context),
-          ),
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back_ios_new_rounded,
-              color: Colors.black,
-              size: isMobile(context) ? null : 40,
-            ),
-          ),
+        appBar: CustomAppBar(
+          title: S.of(context).addTask,
         ),
-        body: const CreateTaskViewBodyBloc(),
+        body: const SafeArea(child: CreateTaskViewBodyBloc()),
       ),
     );
   }
